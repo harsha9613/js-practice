1fa24165dd0034f9a45fc840ab0fe9ad6a6da89f
Harsha vardhan <harshavardhanraj0909@gmail.com>
2025-09-08 19:48:35 +0530

    Added sample.js file

---
+++ sample.js
@@ -0,0 +1,16 @@
+{let name ="Harsha vardhan raj";
+var age = 21;
+const work= "Student";
+{
+    let name ="Eswar";
+    let age ="22";
+    const work ="Developer";
+    console.log("Name:",name);
+    console.log("Age:",age);
+    console.log("Work:",work);
+}
+
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Work:",work);
+}
