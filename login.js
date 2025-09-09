9961e9c75843fb87afbc00d8f8341da56f794a58
Harsha vardhan <harshavardhanraj0909@gmail.com>
2025-09-09 22:37:34 +0530

    login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+const userName="harsha0909";
+const password="Harsha@0909";
+let userNameInput="harsha0909";
+let passwordInput="Harsha@909";
+if(userNameInput==userName && passwordInput==password){
+    console.log("login successful");
+
+}else{
+    console.log("login failed");
+}
