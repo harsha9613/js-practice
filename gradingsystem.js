4839ceee89656c1cf10439f6e833977497e48f3d
Harsha vardhan <harshavardhanraj0909@gmail.com>
2025-09-09 22:54:31 +0530

    gradingsystem.js

---
+++ gradingsystem.js
@@ -0,0 +1,21 @@
+const marks=18;
+if(marks>=90){
+    console.log("A");
+
+}else 
+    if(marks<=89 && marks>=75){
+        console.log("B");
+    }else
+        if(marks<=74 && marks>50){
+            console.log("C");
+        
+        }
+        else
+            if(marks==50){
+                console.log("D");
+
+            }
+            else
+                if(marks<50){
+                    console.log("F");
+                }
